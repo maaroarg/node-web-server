33a3f1b9b413ac871a857ec3859cb10455859e5d
Mario Romero <mario.romero@avantrip.com>
2017-10-13 17:37:14 -0300

    Add project page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Projects Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     code: 500,
